35c4599097cc83ac075a700d31114cfbce885285
yokin86558543 <86558543@qq.com>
2017-12-08 10:23:54 +0800

    剔除了appdateutils类

--- app/src/main/java/com/lining/yokinjar/MainActivity.java
+++ app/src/main/java/com/lining/yokinjar/MainActivity.java
@@ -3,20 +3,28 @@ package com.lining.yokinjar;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SwitchCompat;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.chad.library.adapter.base.BaseItemDraggableAdapter;
 import com.lining.yokinlibrary.utils.ActivityCache;
 import com.lining.yokinlibrary.utils.CL;
+import com.lining.yokinlibrary.utils.ToastUtils;
 
 public class MainActivity extends AppCompatActivity {
     private String TAG=MainActivity.class.getSimpleName();
     TextView click;
+    SwitchCompat switchcompat;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ActivityCache.addActivity(MainActivity.this);
+
+        switchcompat= (SwitchCompat) findViewById(R.id.switchcompat);
 
         click= (TextView) findViewById(R.id.click);
         click.setOnClickListener(new View.OnClickListener() {
@@ -28,11 +36,18 @@ public class MainActivity extends AppCompatActivity {
                 //startActivity(new Intent(MainActivity.this,UserDemo.class));
                 //startActivity(new Intent(MainActivity.this,ErJiDemo.class));
                 //startActivity(new Intent(MainActivity.this,CalendarDemo.class));
-                startActivity(new Intent(MainActivity.this,Demo.class));
+                //startActivity(new Intent(MainActivity.this,Demo.class)); 1+20*4
 
             }
         });
 
+
+        switchcompat.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Toast.makeText(MainActivity.this,"点击了=="+isChecked,Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 
